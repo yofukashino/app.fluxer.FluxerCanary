d2031be3c4fc83b76296c73797ed10fab8a0ae59
yofukashino <73281112+yofukashino@users.noreply.github.com>
2026-08-11 20:58:37 +0530

    update sources

--- generate-sources.sh
+++ generate-sources.sh
@@ -53,17 +53,17 @@ jq '
 ' merged-cargo-sources.json > merged-fixed-cargo-sources.json
 
 jq '
-  if .type == "shell"
-     and (.commands | length == 1)
-     and .commands[0] == "cp -r --reflink=auto \"flatpak-cargo/git/deepfilternet-978576a/libDF\" \"cargo/vendor/deep_filter\""
-  then
-    .commands = [
-      "cp -a flatpak-cargo/git/deepfilternet-978576a/libDF/. cargo/vendor/deep_filter/",
-      "cp -a flatpak-cargo/git/deepfilternet-978576a/models cargo/vendor/"
-    ]
-  else
-    .
-  end
+  map(if .type == "shell"
+      and (.commands | length == 1)
+      and (.commands[0] | test("cp -r --reflink=auto \"flatpak-cargo/git/deepfilternet-[^/]+/libDF\""))
+    then
+      .commands = [
+        "cp -r --reflink=auto flatpak-cargo/git/deepfilternet-*/libDF/* cargo/vendor/deep_filter",
+        "cp -r flatpak-cargo/git/deepfilternet-*/models cargo/vendor"
+      ]
+    else
+      .
+    end)
 ' merged-fixed-cargo-sources.json > merged-fixed-deep_filter-cargo-sources.json
 
 cp "pnpm-sources.json" "$CUR_DIR/pnpm-sources.json"
